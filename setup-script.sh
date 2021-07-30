7f342fc7f205b9a9b1a54788baaaff22fc6d65fc
george <datameshprojects@gmail.com>
2021-07-30 22:05:56 +0100

    updated submission images

--- setup-script.sh
+++ setup-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Variables
-resourceGroup="acdnd-c4-project"
-location="westus2"
+resourceGroup="bartelsmann-rg"
+location="brazilsouth"
 osType="UbuntuLTS"
 vmssName="udacity-vmss"
 adminName="udacityadmin"
